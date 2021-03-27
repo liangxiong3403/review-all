cc97e476b5c1d30e07942e92ed87d6d0dc9fe3b3
liangxiong <liangx@travelsky.com>
2021-03-27 17:24:48 +0800

    excle import and export

--- review-client/src/main/java/org/liangxiong/review/client/controller/UserController.java
+++ review-client/src/main/java/org/liangxiong/review/client/controller/UserController.java
@@ -1,13 +1,21 @@
 package org.liangxiong.review.client.controller;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.ImportParams;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.excel.EasyExcel;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.liangxiong.review.client.dto.UserDTO;
 import org.liangxiong.review.client.entity.User;
+import org.liangxiong.review.client.listener.UploadUserListener;
 import org.liangxiong.review.client.service.IUserService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletOutputStream;
@@ -16,6 +24,7 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 /**
  * @author liangxiong
@@ -37,7 +46,7 @@ public class UserController {
         return userService.list(wrapper);
     }
 
-    @PostMapping("add")
+    @PostMapping("/add")
     public Boolean save(@RequestBody User user) {
         return userService.save(user);
     }
@@ -93,4 +102,27 @@ public class UserController {
         EasyExcel.write(outputStream, User.class).sheet("模板").doWrite(data);
     }
 
+    @PostMapping("/uploadByEasyPoi")
+    @ResponseBody
+    public String uploadByEasyPoi(@RequestParam("file") MultipartFile file) throws Exception {
+        ImportParams importParams = new ImportParams();
+        importParams.setHeadRows(1);
+        List<UserDTO> dtos = ExcelImportUtil.importExcel(file.getInputStream(), UserDTO.class, importParams);
+        if (CollectionUtil.isNotEmpty(dtos)) {
+            List<User> users = dtos.stream().filter(e -> StrUtil.isNotBlank(e.getName())).map(e -> {
+                User user = new User();
+                BeanUtil.copyProperties(e, user);
+                return user;
+            }).collect(Collectors.toList());
+            userService.saveBatch(users);
+        }
+        return "success";
+    }
+
+    @PostMapping("/uploadByEasyExcel")
+    @ResponseBody
+    public String uploadByEasyExcel(@RequestParam("file") MultipartFile file) throws IOException {
+        EasyExcel.read(file.getInputStream(), UserDTO.class, new UploadUserListener(userService)).sheet(0).doRead();
+        return "success";
+    }
 }
