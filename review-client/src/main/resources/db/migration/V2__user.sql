7e10f2b37edaffcff2bd9009efe576fafc0aae11
liangxiong <liangx@travelsky.com>
2021-04-05 18:29:24 +0800

    add dependency for flyway

---
+++ review-client/src/main/resources/db/migration/V2__user.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50728
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50728
+ File Encoding         : 65001
+
+ Date: 05/04/2021 17:39:32
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `ID` bigint(20) NOT NULL,
+  `DELETED` tinyint(1) NULL DEFAULT NULL,
+  `CREATED_TIME` datetime(0) NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime(0) NULL DEFAULT NULL,
+  `UPDATED_BY` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `NAME` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `AGE` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
